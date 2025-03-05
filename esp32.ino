7d3c22cc0bd3f09a6942fd42f3339fcced772f26
destradax <destradax@gmail.com>
2025-03-05 04:10:41 -0500

    adds base websocket client

--- esp32.ino
+++ esp32.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WebServer.h>
+#include <WebSocketsClient.h>
 
 #include "credentials.h"
 #include "webserver.h"
@@ -7,6 +8,26 @@
 
 bool serverStarted = false;
 bool printResult = true;
+
+const char* webSocketHost = "localhost";
+const int webSocketPort = 3001;
+const char* webSocketPath = "/";
+
+WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_CONNECTED:
+      Serial.println("Connected to WebSocket server!");
+      break;
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected from WebSocker server");
+      break;
+    case WStype_TEXT:
+      Serial.printf("Received message: %s\n", payload);
+      break;
+  }
+}
 
 void setup(void) {
   Serial.begin(115200);
@@ -21,6 +42,9 @@ void setup(void) {
 
   if (connectToWifi(ssid, passPhrase)) {
     serverSetup();
+
+    webSocket.begin(webSocketHost, webSocketPort, webSocketPath);
+    webSocket.onEvent(webSocketEvent);
     serverStarted = true;
   }
 }
@@ -28,6 +52,7 @@ void setup(void) {
 void loop(void) {
   if (serverStarted) {
     serverListen();
+    webSocket.loop();
   }
 
   if (printResult) {
